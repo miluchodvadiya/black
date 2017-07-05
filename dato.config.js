23c1d32e47ac0d6e98fbdb073c2ffe1b88000e9f
mitesh <mitesh@pistalix.com>
2017-07-05 09:48:10 +0530

    dato cms configuration

---
+++ dato.config.js
@@ -0,0 +1,118 @@
+/*module.exports = (dato, root, i18n) => {
+  const content = { hello: "world" }
+  root.createDataFile("data/foobar.yml", 'yaml', content)
+}
+
+
+module.exports = (dato, root, i18n) => {
+  root.createPost("content/post/my-post.md", "yaml", {
+    frontmatter: {
+      title: "First article",
+      type: "post",
+      categories: ["random"],
+      weight: 4,
+      date: "2012-04-06",
+    },
+    content: "Lorem **ipsum dolor sit amet**, consectetur adipiscing elit."
+  });
+}
+
+
+module.exports = (dato, root, i18n) => {
+  root.directory("content/post", (dir) => {
+    for (let i = 0; i < 10; i++) {
+      dir.createPost(`post-${i}.md`, "yaml", {
+        frontmatter: {
+          title: `Article ${i}`,
+          type: "post",
+          categories: ["random"],
+          weight: 4,
+          date: "2012-04-06",
+        },
+        content: "Lorem **ipsum dolor sit amet**, consectetur adipiscing elit."
+      });
+    }
+  });
+}
+*/
+
+
+
+/*module.exports = (dato, root, i18n) => {
+
+  // inside a "post" directory...
+  root.directory("content/post", (dir) => {
+
+
+    // ...iterate over the "Blog post" records...
+    dato.blogPosts.forEach((blogPost) => {
+
+      // ...and create a markdown file for each article!
+      root.createPost(`${blogPost.slug}.md`, "yaml", {
+        frontmatter: {
+          title: blogPost.title,
+          type: "post",
+          categories: blogPost.categories.map(cat => cat.slug),
+          date: blogPost.publishedAt,
+        },
+        content: blogPost.content
+      });
+    });
+  });
+}*/
+/*module.exports = (dato, root) => {
+  root.directory('data/', dir => {
+    dir.createDataFile('home.toml', 'toml', {
+      sitename: dato.homepage.sitename,
+      tagline: dato.homepage.tagline,
+      description: dato.homepage.description
+    });
+  });
+}*/
+
+
+module.exports = (dato, root, i18n) => {
+
+
+/*
+  root.directory("content/post", (dir) => {
+    for (let i = 0; i < 10; i++) {
+      dir.createPost(`post-${i}.md`, "yaml", {
+        frontmatter: {
+          title: `Article ${i}`,
+          type: "post",
+          categories: ["random"],
+          weight: 4,
+          date: "2012-04-06",
+        },
+        content: "Lorem **ipsum dolor sit amet**, consectetur adipiscing elit."
+      });
+    }
+  });*/
+
+
+  // inside a "post" directory...
+  root.directory("content/post", (dir) => {
+
+    //console.log(dato);
+    //console.log(dato.posts);
+    // ...iterate over the "Blog post" records...
+    dato.posts.forEach((blogPost) => {
+    // To get the rich text of each post
+    //console.log(blogPost.header.map(head => head.testPageTitle));
+      // ...and create a markdown file for each article!
+      root.createPost(`content/post/${blogPost.slug}.md`, "yaml", {
+        frontmatter: {
+          title: blogPost.title,
+          type: "post",
+          categories: blogPost.categories,
+          weight: blogPost.weight,
+          date: blogPost.date,
+          test_field:blogPost.testField,
+        },
+        content: blogPost.content
+
+      });
+    });
+  });
+}
